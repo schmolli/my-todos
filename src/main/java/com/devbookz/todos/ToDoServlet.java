025fbcb9eed64bb320a6092ab569775f444f5bc5
unknown <martin.schmollinger@gmail.com>
2014-10-28 12:03:41 +0100

    removed bootstrap from servlet and improved logic

--- src/main/java/com/devbookz/todos/ToDoServlet.java
+++ src/main/java/com/devbookz/todos/ToDoServlet.java
@@ -18,105 +18,117 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet({ "/ToDoServlet", "/todos" })
 public class ToDoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private List<ToDo> todos;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ToDoServlet() {
-        super();
-        todos=new Vector<>();
-    }
+	private static final String NOINPUT="Please, enter TODO!";
+	private String errorMessage;
+	private List<ToDo> todos;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//generate output
+	public ToDoServlet() {
+		super();
+		todos = new Vector<>();
+		errorMessage="";
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// generate output
 		processRequest(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// get post parameter
-		String todoDescr = request.getParameter("todo");
-		if (todoDescr!=null&&todoDescr.length()>0) {
-			//Save button
-			//create todo
-			ToDo todo = new ToDo();
-			todo.setDescription(todoDescr);
-			todo.setCreated(new Date());
-			
-			//add todo list
-			todos.add(todo);			
-		} else {
-			//Reset button
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String button = request.getParameter("button");
+
+		switch (button) {
+
+		case "save":
+			// get post parameter
+			String todoDescr = request.getParameter("todo");
+			if (todoDescr != null && todoDescr.length() > 0) {
+				// Save button
+				// create todo
+				ToDo todo = new ToDo();
+				todo.setDescription(todoDescr);
+				todo.setCreated(new Date());
+
+				// add todo list
+				todos.add(todo);
+				errorMessage="";
+			} else {
+				//Error message
+				errorMessage=NOINPUT;
+			}
+			break;
+		case "reset":
+			// Reset button
 			todos.clear();
+			errorMessage="";
 		}
-		//generate output
+		// generate output
 		processRequest(request, response);
 	}
-		
 
-	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		response.setCharacterEncoding("utf-8");
 		response.getOutputStream().print(generateOutput());
 	}
-	
+
 	private String generateTable() {
 		StringBuffer table = new StringBuffer();
 		for (ToDo todo : todos) {
-			table.append("<tr><td>"+todo.getDescription()+"</td>");
-			table.append("<td>"+todo.getCreated()+"</td></tr>");
+			table.append("<tr><td>" + todo.getDescription() + "</td>");
+			table.append("<td>" + todo.getCreated() + "</td></tr>");
 		}
-		if (table.length()==0) table.append("<tr><td>Currently, there are no TODOs for you!</td></tr>");
+		if (table.length() == 0)
+			table.append("<tr></tr>");
 		return table.toString();
 	}
-	
+
 	private String generateOutput() {
 		StringBuffer site = new StringBuffer();
-		
-		site.append("<!DOCTYPE html>" +
-		"<html>" +
-		"<head>" +
-			"<meta charset=\"UTF-8\">" +
-			"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">" +
-			"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">" +
-			"<title>My TODOs</title>" +
-			"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css\">" +
-		"</head>" +
-		"<body>" +
-		"<div class=\"container\"" +
-			"<section>" +
-				"<h1>Enter TODO</h1>" +
-				"<form role=\"form\" method=\"POST\" action=\"\">" +
-		  			"<div class=\"form-group\">" +
-						"<input type=\"text\" name=\"todo\" class=\"form-control\" size=\"50\" placeholder=\"Enter todo\" />" + 
-						"<button type=\"submit\" id=\"save\" class=\"btn btn-default\">Save</button>" +
-					"</div>" +
-				"</form>" +
-			"</section>" +
-			"<section>" +
-				"<h1>My TODOs</h1>" +
-				"<table class=\"table table-striped\">" +
-					generateTable() +
-				"</table>" +
-				"<form role=\"form\" action=\"\">" +
-					"<div class=\"form-group\">" + 
-						"<button type=\"submit\" id=\"reset\" formmethod=\"POST\" class=\"btn btn-default\">Reset</button>"+
-					"</div>" +
-				"</form>" +
-			"</section>" +
-			"<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js\"></script>" +
-			"</div" +
-			"</body>" +
-		"</html>");
-		
-		
+
+		site.append("<!DOCTYPE html>"
+				+ "<html>"
+				+ "<head>"
+				+ "<meta charset=\"UTF-8\">"
+				+ "<title>My TODOs</title>"
+				+ "<link rel=\"stylesheet\" href=\"\">"
+				+ "</head>"
+				+ "<body>"
+				+ "<div id=\"header\">"
+				+ "<p><b>My-TODOs</b> - Little Helper</p>"
+				+ "</div>"
+				+ "<div id=\"container\""
+				+ "<div id=\"content\">"
+				+ "<h1>Enter TODO</h1>"
+				+ "<form  method=\"POST\" action=\"\">"
+				+ "<input type=\"text\" name=\"todo\" class=\"form-control\" size=\"50\" placeholder=\"Enter todo\" />"
+				+ "<button type=\"submit\" name=\"button\" value=\"save\">Save</button>" 
+				+ errorMessage 
+				+ "<h1>My TODOs</h1>"
+				+ "<table>"
+				+ generateTable()
+				+ "</table>"
+				+ "<button type=\"submit\" name=\"button\" value=\"reset\">Reset</button>"
+				+ "</form>" + "</div>" + "<div id=\"footer\">"
+				+ "<p>(C) 2014 geakleap ltd., MIT Licence</p>" + "</div>"
+				+ "</body>" + "</html>");
+
 		return site.toString();
-		
+
 	}
 
 }
