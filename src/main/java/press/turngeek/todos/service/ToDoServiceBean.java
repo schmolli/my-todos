f419f16118a6ea42845f4b3db64f441e66049dc1
J-EE <martin.schmollinger@gmail.com>
2014-12-30 11:44:04 +0100

    changed name of local variable

--- src/main/java/press/turngeek/todos/service/ToDoServiceBean.java
+++ src/main/java/press/turngeek/todos/service/ToDoServiceBean.java
@@ -12,23 +12,23 @@ import press.turngeek.todos.model.ToDo;
 public class ToDoServiceBean implements ToDoService {
 	
 	@EJB
-	ToDoManager provider;
+	ToDoManager manager;
 
 	@Override
 	public List<ToDo> getAllToDos() {
-		List<ToDo> todos = provider.findAllToDos();
+		List<ToDo> todos = manager.findAllToDos();
 		return todos;
 	}
 
 	@Override
 	public void addToDo(ToDo todo) {
-		List<ToDo> todos = provider.findAllToDos();
+		List<ToDo> todos = manager.findAllToDos();
 		todos.add(todo);
 	}
 
 	@Override
 	public void resetToDos() {
-		List<ToDo> todos = provider.findAllToDos();
+		List<ToDo> todos = manager.findAllToDos();
 		todos.clear();
 	}
 }
